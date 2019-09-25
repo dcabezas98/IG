4cbf72792dcca0ec3cc5fe9f31a3248f98962d4f
David Cabezas Berrido <dxabezas@gmail.com>
2019-09-25 20:39:35 +0200

    Practica 1 empezada

---
+++ trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dcabezas/Documents/git/IG/recursos/src/cauce.cpp" "/home/dcabezas/Documents/git/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/recursos/src/cauce.cpp.o"
+  "/home/dcabezas/Documents/git/IG/recursos/src/fbo.cpp" "/home/dcabezas/Documents/git/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/recursos/src/fbo.cpp.o"
+  "/home/dcabezas/Documents/git/IG/recursos/src/ig-aux.cpp" "/home/dcabezas/Documents/git/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/recursos/src/ig-aux.cpp.o"
+  "/home/dcabezas/Documents/git/IG/recursos/src/lector-jpg.cpp" "/home/dcabezas/Documents/git/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/recursos/src/lector-jpg.cpp.o"
+  "/home/dcabezas/Documents/git/IG/recursos/src/lector-ply.cpp" "/home/dcabezas/Documents/git/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/recursos/src/lector-ply.cpp.o"
+  "/home/dcabezas/Documents/git/IG/recursos/src/matrices-tr.cpp" "/home/dcabezas/Documents/git/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/recursos/src/matrices-tr.cpp.o"
+  "/home/dcabezas/Documents/git/IG/trabajo/src/animacion.cpp" "/home/dcabezas/Documents/git/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/trabajo/src/animacion.cpp.o"
+  "/home/dcabezas/Documents/git/IG/trabajo/src/camara.cpp" "/home/dcabezas/Documents/git/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/trabajo/src/camara.cpp.o"
+  "/home/dcabezas/Documents/git/IG/trabajo/src/escena.cpp" "/home/dcabezas/Documents/git/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/trabajo/src/escena.cpp.o"
+  "/home/dcabezas/Documents/git/IG/trabajo/src/grafo-escena.cpp" "/home/dcabezas/Documents/git/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/trabajo/src/grafo-escena.cpp.o"
+  "/home/dcabezas/Documents/git/IG/trabajo/src/main.cpp" "/home/dcabezas/Documents/git/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/trabajo/src/main.cpp.o"
+  "/home/dcabezas/Documents/git/IG/trabajo/src/malla-ind.cpp" "/home/dcabezas/Documents/git/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/trabajo/src/malla-ind.cpp.o"
+  "/home/dcabezas/Documents/git/IG/trabajo/src/malla-revol.cpp" "/home/dcabezas/Documents/git/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/trabajo/src/malla-revol.cpp.o"
+  "/home/dcabezas/Documents/git/IG/trabajo/src/materiales-luces.cpp" "/home/dcabezas/Documents/git/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/trabajo/src/materiales-luces.cpp.o"
+  "/home/dcabezas/Documents/git/IG/trabajo/src/objeto3d.cpp" "/home/dcabezas/Documents/git/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/trabajo/src/objeto3d.cpp.o"
+  "/home/dcabezas/Documents/git/IG/trabajo/src/seleccion.cpp" "/home/dcabezas/Documents/git/IG/trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/trabajo/src/seleccion.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "LINUX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../../trabajo/include"
+  "../../../recursos/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ trabajo/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/trabajo/src/animacion.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/trabajo/src/camara.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/trabajo/src/escena.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/trabajo/src/grafo-escena.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/trabajo/src/main.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/trabajo/src/malla-ind.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/trabajo/src/malla-revol.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/trabajo/src/materiales-luces.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/trabajo/src/objeto3d.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/trabajo/src/seleccion.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/recursos/src/cauce.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/recursos/src/fbo.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/recursos/src/ig-aux.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/recursos/src/lector-jpg.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/recursos/src/lector-ply.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/dcabezas/Documents/git/IG/recursos/src/matrices-tr.cpp.o"
+  "pracs_ig_exe.pdb"
+  "pracs_ig_exe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pracs_ig_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
